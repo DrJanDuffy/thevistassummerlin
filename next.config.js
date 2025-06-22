9f78ef052101f518d32ddaf7fc7e007177ade2e2
genekellyboyle <geneboyle@gmail.com>
2025-06-22 07:11:49 -0700

    fix(build): configure transpilePackages for react-map-gl

--- next.config.js
+++ next.config.js
@@ -1,18 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // transpilePackages: ['react-map-gl', 'mapbox-gl'], // This is no longer the recommended approach
+  transpilePackages: ['react-map-gl'],
   experimental: {
     // appDir: true, // Uncomment if you use the App Router (Next.js 13+)
     // serverActions: true, // Uncomment if you use Server Actions
   },
   images: {
     domains: ['placehold.co', 'api.mapbox.com'],
-  },
-  webpack: (config) => {
-    config.externals = [...config.externals, 'mapbox-gl'];
-    // config.resolve.alias['mapbox-gl'] = 'mapbox-gl/dist/mapbox-gl.js'; // This is also not needed with the externals config
-    return config;
   },
   // i18n: {
   //   locales: ['en'],
